5ec814cdef24e5dba5c680ffe5ca032fbe4cc639
Paweł Fijałkowski <fijalkowskipablo@gmail.com>
2021-01-09 16:09:13 +0100

    working compiling

--- compilator.c
+++ compilator.c
@@ -8,6 +8,9 @@
 #define EMPTY_STACK -1
 #define BASE 10
 #define READ '='
+#define OPEN_BRACKETS '('
+#define CLOSE_BRACKETS ')'
+
 
 
 enum Instruction {
@@ -18,7 +21,7 @@ enum Instruction {
 typedef struct Entry{
 
 	int id;
-	enum Instruction Instruction;
+	enum Instruction instruction;
 	char *parameter1;
 	char *parameter2;
 
@@ -37,6 +40,7 @@ typedef struct Variables{
 typedef struct Looper { 
 
 	Entry* entries;
+	int entries_size;
 	int entries_n;
 	Variables vals;
 }Looper;
@@ -150,11 +154,13 @@ Looper initializeLooper(){
 	Looper l;
 	l.vals = initializeVariables();
 	l.entries_n = 0;
+	l.entries_size = 1;
+	l.entries = malloc(sizeof(Entry));
 	return l;
 }
 
 
-char* line(bool *isEnd){
+char* line(int *len, bool *isEnd){
 	char* res = malloc(sizeof(char));
 	int letters = 0, size = 1;
 	char c;
@@ -169,8 +175,160 @@ char* line(bool *isEnd){
 	if(c == EOF){
 		*isEnd = true;
 	}
+	*len = letters;
 	return res;
 }
+
+void addEntry(Entry e,Looper *l){
+	if(l->entries_size == l->entries_n){
+		l->entries = realloc(l->entries,(unsigned)2*(l->entries_size)*sizeof(Entry));
+		l->entries_size = 2*(l->entries_size);
+	}
+	l->entries[l->entries_n] = e;
+	l->entries_n += 1;
+}
+
+Entry createEntry(int id, enum Instruction i, char* parameter1, char* parameter2){
+	Entry e;
+	e.id = id;
+	e.instruction = i;
+	e.parameter1 = parameter1; 
+	e.parameter2 = parameter2;	
+	return e;
+}
+
+void printEntries(Looper *l){
+	printf("--------------------------\n");
+	for(int i = 0; i < l->entries_n; i++){
+		Entry e = l->entries[i];
+		char *pol;
+		switch(e.instruction){
+			case ADD:
+				pol = "ADD";
+				break;
+			case INC:
+				pol = "INC";
+				break;
+			case JMP:	
+				pol = "JMP";
+				break;
+			case DJZ:
+				pol = "DJZ";
+				break;
+			case HLT:
+				pol = "HLT";
+				break;
+			case CLR:
+				pol = "CLR";
+				break;
+		}
+		char* val1 = "";
+		char* val2 = "";
+		if(e.parameter1 != NULL){
+			val1 = e.parameter1;
+		}
+		if(e.parameter2 != NULL){
+			val2 = e.parameter2;
+		}
+		printf("%3d.| %3s | %7s | %7s |\n", e.id,pol,val1,val2);
+	}
+	printf("--------------------------\n");
+
+
+}
+
+
+int countDigits(int a){
+	int count = 0;
+	do
+    {
+        count++;
+        a /= BASE;
+    }while(a != 0);
+    return count;
+}
+
+char* intToCharPointer(int a){
+	int n = countDigits(a);
+	char *num = malloc((unsigned) n*sizeof(char));
+	int i = n - 1;
+	do
+    {
+        num[i] = a%BASE + '0';
+        a /= BASE;
+        i--;
+    }while(i >= 0);
+    return num;
+}
+
+void initializeStack(Stack *s){
+	s->content = malloc(sizeof(int));
+	s->stack_size = 1;
+	s->content_size = 0;
+}
+
+
+
+
+void compileLine(Looper *l,char* cline,int n){
+	Stack s;
+	initializeStack(&s);
+	int i = 0;
+	while(i < n){
+		if(cline[i] == OPEN_BRACKETS){
+			int j = i + 1;
+			while(cline[j] != OPEN_BRACKETS && cline[j] != CLOSE_BRACKETS){
+				j++;
+			}
+			bool optimizable = (cline[j] == CLOSE_BRACKETS);
+			char *mainVariable = malloc((unsigned)sizeof(char));
+			mainVariable[0] = cline[++i];
+			if(optimizable){
+				i++;
+				while(i < j){
+					char *variable_to_add = malloc((unsigned)sizeof(char));
+					variable_to_add[0] = cline[i];
+					Entry e = createEntry(l->entries_n,ADD,variable_to_add,mainVariable);
+					addEntry(e,l);
+					i++;
+					}
+				Entry closing = createEntry(l->entries_n,CLR,mainVariable,NULL);
+				addEntry(closing,l);
+				i++;
+			}else{ 
+				pushStack(&s,l->entries_n);
+				i++;
+				Entry djz = createEntry(l->entries_n,DJZ,mainVariable,NULL);
+				addEntry(djz,l);
+				while(i < j){
+					char *variable_to_inc = malloc((unsigned)sizeof(char));
+					variable_to_inc[0] = cline[i];
+					Entry e_inc = createEntry(l->entries_n,INC,variable_to_inc,NULL);
+					addEntry(e_inc,l);
+					i++;
+				}
+			}
+		}else if(cline[i] == CLOSE_BRACKETS){
+			int j = popStack(&s);
+			l->entries[j].parameter2 = intToCharPointer(l->entries_n+1);
+			Entry e_jmp = createEntry(l->entries_n,JMP,intToCharPointer(j),NULL);
+			addEntry(e_jmp,l);
+			i++;
+		}else{
+
+			char *variable_to_inc = malloc((unsigned)sizeof(char));
+			variable_to_inc[0] = cline[i];
+			Entry e_inc = createEntry(l->entries_n,INC,variable_to_inc,NULL);
+			addEntry(e_inc,l);
+			i++;
+		}
+	}
+	addEntry(createEntry(l->entries_n,HLT,NULL,NULL),l);
+	printEntries(l);
+
+}
+
+
 
 
 
@@ -178,12 +336,13 @@ void read(Looper *l){
 	
 	bool isEnd = false;
 	while(!isEnd){
-		char* currentLine = line(&isEnd);
+		int len;
+		char* currentLine = line(&len,&isEnd);
 		// line analysis here
 		if(currentLine[0] == READ){
 			printVariableValue(&l->vals,currentLine[1]);
 		}else{
-			printf("Ble ble");
+			compileLine(l,currentLine,len);
 		}
 	}
 	
